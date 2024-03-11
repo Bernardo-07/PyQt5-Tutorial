09dbaf13ba86917386543ea36069da1f1bb1fe04
Bernardo-07 <bernardoduarte271@gmail.com>
2024-03-11 09:47:12 -0300

    basic window creation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 400, 400)
+    win.setWindowTitle("Let him Cook!")
+    win.show()
+    sys.exit(app.exec())
+
+window()
