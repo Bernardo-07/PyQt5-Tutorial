d6562f03d5d64c719a7f68451d1ae28e9681a30b
Bernardo-07 <bernardoduarte271@gmail.com>
2024-03-11 10:19:28 -0300

    buttons and events (signals)

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 400, 400)
+        self.setWindowTitle("Let him Cook!")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello world")
+        self.label.move(50, 50)
+
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Click me?")
+        self.button.clicked.connect(self.action)
+
+    def action(self):
+        print("Clicked")
+        self.label.setText("you pressed the button")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 400, 400)
-    win.setWindowTitle("Let him Cook!")
-    body(win)
+    win = MyWindow()
+
     win.show()
     sys.exit(app.exec())
-
-def body(win):
-    label = QtWidgets.QLabel(win)
-    label.setText("Hello world")
-    label.move(50, 50)
-
-    button = QtWidgets.QPushButton(win)
-    button.setText("Click me?")
-    button.clicked.connect(action)
-
-def action():
-    print("Clicked")
 
 window()
