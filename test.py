a535fa82161e4da6bea9dea0b15f9e1a587b19cf
Bernardo-07 <bernardoduarte271@gmail.com>
2024-03-13 10:48:01 -0300

    Shortcuts, Status Bar and Triggers

--- test.py
+++ test.py
@@ -17,26 +17,52 @@ class Ui_MainWindow(object):
         MainWindow.resize(451, 298)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.button1 = QtWidgets.QPushButton(self.centralwidget)
         self.button1.setGeometry(QtCore.QRect(130, 180, 181, 51))
         self.button1.setObjectName("button1")
+
         self.label1 = QtWidgets.QLabel(self.centralwidget)
         self.label1.setGeometry(QtCore.QRect(50, 40, 81, 21))
         self.label1.setObjectName("label1")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 451, 21))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+
         MainWindow.setStatusBar(self.statusbar)
+        self.actionNew = QtWidgets.QAction(MainWindow)
+        self.actionNew.setObjectName("actionNew")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionCopy = QtWidgets.QAction(MainWindow)
+        self.actionCopy.setObjectName("actionCopy")
+        self.actionPaste = QtWidgets.QAction(MainWindow)
+        self.actionPaste.setObjectName("actionPaste")
+
+        self.menuFile.addAction(self.actionNew)
+        self.menuFile.addAction(self.actionSave)
+        self.menuEdit.addAction(self.actionCopy)
+        self.menuEdit.addAction(self.actionPaste)
         self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.actionCopy.triggered.connect(lambda: self.clicked("Copy was clicked"))
+        self.actionPaste.triggered.connect(lambda: self.clicked("Paste was clicked"))
+        self.actionNew.triggered.connect(lambda: self.clicked("New was clicked"))
+        self.actionSave.triggered.connect(lambda: self.clicked("Save was clicked"))
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -44,6 +70,27 @@ class Ui_MainWindow(object):
         self.button1.setText(_translate("MainWindow", "click me please"))
         self.label1.setText(_translate("MainWindow", "I\'m Batman"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+
+        self.actionNew.setText(_translate("MainWindow", "New"))
+        self.actionNew.setStatusTip(_translate("MainWindow", "Create a new file"))
+        self.actionNew.setShortcut(_translate("MainWindow", "Ctrl+N"))
+
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionSave.setStatusTip(_translate("MainWindow", "Save the file"))
+        self.actionSave.setShortcut(_translate("MainWindow", "Ctrl+S"))
+
+        self.actionCopy.setText(_translate("MainWindow", "Copy"))
+        self.actionCopy.setStatusTip(_translate("MainWindow", "Copy a file"))
+        self.actionCopy.setShortcut(_translate("MainWindow", "Ctrl+C"))
+
+        self.actionPaste.setText(_translate("MainWindow", "Paste"))
+        self.actionPaste.setStatusTip(_translate("MainWindow", "Paste a file"))
+        self.actionPaste.setShortcut(_translate("MainWindow", "Ctrl+V"))
+
+    def clicked(self, text):
+        self.label1.setText(text)
+        self.label1.adjustSize()
 
 
 if __name__ == "__main__":
